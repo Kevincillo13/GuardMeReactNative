2010b5a82706e54981af1d4b8f62c9295f612ef4
Kevincillo13 <crkevin105@gmail.com>
2024-07-01 22:05:57 -0600

    Color texto status bar

--- routes/AuthStack.tsx
+++ routes/AuthStack.tsx
@@ -1,17 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import useAuth from "../hooks/useAuth";
 import LoginPage from "../pages/LoginPage";
 import SignUpPage from "../pages/SingUpPage";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 function AuthStack() {
-  const {authData, login, logout} = useAuth()
+  const { authData, login, logout } = useAuth();
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="login" component={LoginPage}/>
-      <Stack.Screen name="singUp" component={SignUpPage}/>
-    </Stack.Navigator>
-  )
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" component={LoginPage} />
+        <Stack.Screen name="singUp" component={SignUpPage} />
+      </Stack.Navigator>
+    </>
+  );
 }
-export default AuthStack
+
+export default AuthStack;
