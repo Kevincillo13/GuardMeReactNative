6fdbdab41dfc34d0214293569cfbdaf98624529a
eldenais <cordobagarciaroberto.04@gmail.com>
2024-07-03 09:57:00 -0600

    Local host update

--- constants/auth.constants.ts
+++ constants/auth.constants.ts
@@ -1,5 +1,5 @@
 const lifeSessionTimeInMin = 1
 
-const authHost = "http://192.168.100.52:3000/GuardMeAPI/"
+const authHost = "http://LocalHost:3000/GuardMeAPI/"
 
 export {lifeSessionTimeInMin, authHost} 
