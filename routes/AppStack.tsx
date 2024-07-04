4e8db3fbabfcd0e65e6c6bb9389dfdd8500f8d05
Kevincillo13 <crkevin105@gmail.com>
2024-07-03 21:02:19 -0600

    Tab navigation bar

--- routes/AppStack.tsx
+++ routes/AppStack.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
 import HomePage from '../pages/HomePage';
 import PatientPage from '../pages/PatientPage';
 import QRPage from '../pages/QRPage';
@@ -18,18 +19,101 @@ const screenOptions = {
     right: 0,
     left: 0,
     elevation: 0,
-    height: 60,
-    backgroundColor: "#fff"
-  }
+    height: 80,
+    backgroundColor: "#fff",
+    borderTopColor: "#0f74d3",
+    borderTopWidth: 0,
+    paddingBottom: 15,
+  },
 };
 
 function BottomTabs() {
   return (
     <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen name="home" component={HomePage} />
-      <Tab.Screen name="patient" component={PatientPage} />
-      <Tab.Screen name="qr" component={QRPage} />
-      <Tab.Screen name="profile" component={ProfilePage} />
+      <Tab.Screen 
+        name="home" 
+        component={HomePage} 
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 40,
+              width: 40,
+              borderRadius: 25,
+              backgroundColor: focused ? '#0f74d3' : 'transparent',
+              paddingTop: 8,
+              paddingBottom: 10,
+              transform: [{ scale: focused ? 1.2 : 1 }],
+            }}>
+              <FontAwesome name="home" size={24} color={focused ? 'white' : '#0f74d3'} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="patient" 
+        component={PatientPage}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 40,
+              width: 40,
+              borderRadius: 25,
+              backgroundColor: focused ? '#0f74d3' : 'transparent',
+              paddingTop: 8,
+              paddingBottom: 10,
+              transform: [{ scale: focused ? 1.2 : 1 }],
+            }}>
+              <FontAwesome name="user-md" size={24} color={focused ? 'white' : '#0f74d3'} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="qr" 
+        component={QRPage}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 40,
+              width: 40,
+              borderRadius: 25,
+              backgroundColor: focused ? '#0f74d3' : 'transparent',
+              paddingTop: 8,
+              paddingBottom: 10,
+              transform: [{ scale: focused ? 1.2 : 1 }],
+            }}>
+              <FontAwesome name="qrcode" size={24} color={focused ? 'white' : '#0f74d3'} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="profile" 
+        component={ProfilePage}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 40,
+              width: 40,
+              borderRadius: 25,
+              backgroundColor: focused ? '#0f74d3' : 'transparent',
+              paddingTop: 8,
+              paddingBottom: 10,
+              transform: [{ scale: focused ? 1.2 : 1 }],
+            }}>
+              <FontAwesome name="user" size={24} color={focused ? 'white' : '#0f74d3'} />
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
